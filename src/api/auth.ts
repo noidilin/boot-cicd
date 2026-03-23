80fa7185d8a6c43b6ca12fc61d984bd3ba4a3c65
noidilin <linganinja.0120@gmail.com>
2026-03-23 23:03:31 +0800

    feat: break the code intentionally for testing ci

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,5 +1,3 @@
-import { IncomingHttpHeaders } from "http";
-
 export function getAPIKey(headers: IncomingHttpHeaders): string | null {
   const authHeader = headers["authorization"];
   if (!authHeader) {
