0a75a761cc5ebd84217cd020d478c0dc6b517f72
noidilin <linganinja.0120@gmail.com>
2026-03-23 23:04:19 +0800

    fix: revert "feat: break the code intentionally for testing ci"
    This reverts commit 54bd50acb264a204bb20997a2af15892cab12766.

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,3 +1,5 @@
+import { IncomingHttpHeaders } from "http";
+
 export function getAPIKey(headers: IncomingHttpHeaders): string | null {
   const authHeader = headers["authorization"];
   if (!authHeader) {
